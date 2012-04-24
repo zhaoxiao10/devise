66972078cae53572b997b9765c1cf40fedc5a6dc
zhaoyan <zhaoyan@zhaoyan-Latitude-D630.(none)>
2012-04-24 23:29:49 +0800

    add main_app.root_path

--- lib/devise/controllers/helpers.rb
+++ lib/devise/controllers/helpers.rb
@@ -179,7 +179,7 @@ module Devise
         if respond_to?(home_path, true)
           send(home_path)
         elsif respond_to?(:root_path)
-          root_path
+          main_app.root_path
         else
           "/"
         end
